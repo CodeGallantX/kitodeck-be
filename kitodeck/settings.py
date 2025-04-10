6dfe2781fd37a1b923190f92c948fa995011b036
John A. Samuel <johnayobami77@proton.me>
2025-04-11 00:37:47 +0100

    Postgresql processes

--- kitodeck/settings.py
+++ kitodeck/settings.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from decouple import config
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +90,7 @@ WSGI_APPLICATION = 'kitodeck.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
 
 # DATABASES = {
