7512c2be141b6f21a9d72a682c647018639d30e0
John A. Samuel <144686157+CodeGallantX@users.noreply.github.com>
2025-04-11 00:49:09 +0100

    Successfully configured postgresql

--- kitodeck/settings.py
+++ kitodeck/settings.py
@@ -90,7 +90,9 @@ WSGI_APPLICATION = 'kitodeck.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 # DATABASES = {
